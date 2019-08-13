280b555ae6bbde5f4a166bfec07f9ec647d83cc0
Znogoud <znogoud3@gmail.com>
2019-08-13 17:31:56 +0300

    hui

--- shortest-word.js
+++ shortest-word.js
@@ -1,2 +1,12 @@
-const str = "dksal jdakls jdklsaj dkalsd";
-const words = str.split(" ");
+function findShort(s) {
+    const words = s.split(" ");
+    let x = words[0].length;
+    for (let i = 1; i < words.length; i++) {
+        if (words[i].length < x) {
+            x = words[i].length;
+        }
+    }
+    return x;
+}
+
+
