84d01c84934b7e8ffc4032ad96b31cc5a51fc8ea
Znogoud <znogoud3@gmail.com>
2019-08-23 19:12:31 +0300

    indexOf!!!

--- remove-duplicate-words.js
+++ remove-duplicate-words.js
@@ -1,11 +1,13 @@
 function removeDuplicateWords(s) {
     let arr = s.split(" ");
+    let result = [];
     for (let i = 0; i < arr.length; i++) {
-        if (arr[i] === arr[i+1]) {
-            arr.splice(arr[i]);
-            return arr;
-        } 
+        if (result.indexOf(arr[i]) === -1) {
+            result.push(arr[i]);
+        }
     }
+    return (result.join(' '));
 }
 
 //removeDuplicateWords('alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta')
+
