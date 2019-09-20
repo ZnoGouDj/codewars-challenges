bda4d34d3f396101bb258f8c3fd60b06df0b7f8e
Egor <znogoud3@gmail.com>
2019-09-20 17:18:31 +0300

    feature: new katas

---
+++ 7kyu-square-every-digit.js
@@ -0,0 +1,23 @@
+// Welcome. In this kata, you are asked to square every digit of a number.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 9^2 is 81 and 1^2 is 1.
+
+// Note: The function accepts an integer and returns an integer
+// Test.assertEquals(squareDigits(9119), 811181);
+
+function squareDigits(num) {
+    let numArr = Array.from(num.toString());
+    let arr = numArr.map(num => Math.pow(num, 2));
+    return Number(arr.join(''));
+}
+
+//top
+
+function squareDigits(num) {
+    return Number(('' + num).split('').map(function (val) { return val * val; }).join(''));
+
+}
+
+//shortest
+
+const squareDigits = (num) => Number((num + '').split("").map(c => c * c).join(""));
