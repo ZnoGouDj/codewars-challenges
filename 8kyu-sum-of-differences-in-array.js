76fd79ee5b1e6ae4c7c47784bae639b983e4b8b9
Egor <znogoud3@gmail.com>
2019-09-30 11:47:11 +0300

    feat: reach 5 kyu in codewars

---
+++ 8kyu-sum-of-differences-in-array.js
@@ -0,0 +1,19 @@
+// Your task is to sum the differences between consecutive 
+// pairs in the array in descending order.
+
+// For example: sumOfDifferences([1, 2, 10]) Returns 9
+
+// Descending order: [10, 2, 1]
+
+// Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
+
+// If the array is empty or the array has only one element the result should be 0.
+
+function sumOfDifferences(arr) {
+    arr.sort((a, b) => b - a);
+    return arr.length > 0 ? arr[0] - arr[arr.length - 1] : 0;
+}
+//top
+function sumOfDifferences(arr) {
+    return arr.length > 1 ? Math.max(...arr) - Math.min(...arr) : 0;
+}
