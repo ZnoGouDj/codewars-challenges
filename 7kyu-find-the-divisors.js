00d16487d7e1811523545efa5d4d4ff2a0509f0d
ZnoGouDj <znogoud3@gmail.com>
2022-02-14 16:30:23 +0300

    new kata

---
+++ 7kyu-find-the-divisors.js
@@ -0,0 +1,15 @@
+function divisors(integer) {
+  const divisors = [];
+
+  for (let i = 2; i < integer; i++) {
+    if (integer % i === 0) {
+      divisors.push(i);
+    }
+  }
+
+  return divisors.length ? divisors : `${integer} is prime`;
+}
+
+console.log(divisors(12)); // should return [2,3,4,6]
+console.log(divisors(25)); // should return [5]
+console.log(divisors(13)); // should return "13 is prime"
