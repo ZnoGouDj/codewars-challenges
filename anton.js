07b020f0b0dd5287cb0c12dd4c580c9506cb22bc
ZnoGouDj <znogoud3@gmail.com>
2022-02-15 22:02:33 +0300

    anton kata

---
+++ anton.js
@@ -0,0 +1,48 @@
+function divide(number, divideBy) {
+  let num = number;
+  let arr = [];
+
+  for (let i = 0; i < divideBy - 1; i++) {
+    let random = getRandomIntInclusive(0, num);
+    num -= random;
+    arr.push(random);
+  }
+
+  arr.push(num);
+
+  return arr;
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(divideWithFractions(15, 3));
+
+//! ============================================================
+
+function divideWithFractions(number, divideBy) {
+  let num = number * 100;
+  let arr = [];
+
+  for (let i = 0; i < divideBy - 1; i++) {
+    let random = getRandomIntInclusiveFractions(0, num);
+
+    num -= random;
+    arr.push(random);
+  }
+
+  arr.push(num);
+
+  return arr.map(el => el / 100);
+}
+
+function getRandomIntInclusiveFractions(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+
+  let random = Math.floor(Math.random() * (max - min + 1)) + min;
+  return random % 5 === 0 ? random : getRandomIntInclusiveFractions(min, max);
+}
