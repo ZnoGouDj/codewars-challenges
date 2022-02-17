b019a8a9ff2825fce67ef4294bada4efc157a014
ZnoGouDj <znogoud3@gmail.com>
2022-02-17 13:53:11 +0300

    2 katas

---
+++ 6kyu-sequences-and-series.js
@@ -0,0 +1,25 @@
+function getScore(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum += i * 50;
+  }
+
+  return sum;
+}
+
+function getScore(n) {
+  return n !== 0 ? n * 50 + getScore(n - 1) : 0;
+}
+
+/*
+
+n | score
+---+-------
+ 1 |  50
+ 2 |  150
+ 3 |  300
+ 4 |  500
+ 5 |  750
+
+ */
