a169a00dab92cc971e33dce8a922a15c550beb58
ZnoGouDj <znogoud3@gmail.com>
2022-02-24 16:36:34 +0300

    6kyu

---
+++ 6kyu-build-tower.js
@@ -0,0 +1,44 @@
+function towerBuilder(nFloors) {
+    const arr = [];
+
+    let mountain = '*';
+    let emptiness = '';
+    let stringToPush = '';
+
+    for (let i = 1; i <= nFloors; i++) {
+        for (let j = nFloors - i; j > 0; j--) {
+            emptiness += ' ';
+        }
+        stringToPush += emptiness + mountain + emptiness;
+        arr.push(stringToPush)
+        stringToPush = '';
+        emptiness = '';
+        mountain += '**';
+    }
+
+    return arr;
+}
+
+function towerBuilder(n) {
+    return Array.from({ length: n }, function (v, k) {
+        const spaces = ' '.repeat(n - k - 1);
+        return spaces + '*'.repeat(k + k + 1) + spaces;
+    });
+}
+
+console.log(towerBuilder(1), ["*"]);
+console.log(towerBuilder(2), [" * ", "***"]);
+console.log(towerBuilder(3), ["  *  ", " *** ", "*****"]);
+
+/*
+
+[
+  "     *     ", 
+  "    ***    ", 
+  "   *****   ", 
+  "  *******  ", 
+  " ********* ", 
+  "***********"
+]
+
+*/
