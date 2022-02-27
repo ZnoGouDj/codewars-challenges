8e29e7e67bf6e6b10c2a87c33c8a1b5dee3ed7c0
ZnoGouDj <znogoud3@gmail.com>
2022-02-27 14:45:59 +0300

    6kyu/8kyu

---
+++ 6kyu-replace-with-alphabet-position.js
@@ -0,0 +1,57 @@
+function alphabetPosition(text) {
+  const positions = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  let outputString = '';
+
+  let textSplit = text.split('');
+
+  for (let i = 0; i < textSplit.length; i++) {
+    for (let j = 0; j < positions.length; j++) {
+      if (textSplit[i].toLowerCase() === positions[j]) {
+        outputString += j + 1 + ' ';
+      }
+    }
+  }
+
+  return outputString.trim();
+}
+
+//!
+
+function alphabetPosition(text) {
+  return text
+    .toUpperCase()
+    .match(/[a-z]/gi)
+    .map(c => c.charCodeAt() - 64)
+    .join(' ');
+}
+
+console.log(alphabetPosition("The sunset sets at twelve o' clock.")); // "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+console.log(alphabetPosition('The narwhal bacons at midnight.')); // "20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20"
---
+++ 8kyu-sum-without-something.js
@@ -0,0 +1,19 @@
+function sumArray(array) {
+  return array && array.length > 2
+    ? [...array]
+        .sort((a, b) => a - b)
+        .splice(1, -1)
+        .reduce((a, b) => a + b, 0)
+    : 0;
+}
+
+/*
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+*/
+
+console.log(sumArray(null)); // 0
+console.log(sumArray([])); // 0
+console.log(sumArray([3])); // 0
+console.log(sumArray([3, 5])); // 0
+console.log(sumArray([6, 2, 1, 8, 10])); // 16
