851d371e96d4065abd12555407d145212a7d9a0c
ZnoGouDj <znogoud3@gmail.com>
2022-03-01 23:13:06 +0300

    6kyu

---
+++ 6kyu-count-the-smiley-faces.js
@@ -0,0 +1,27 @@
+//return the total number of smiling faces in the array
+function countSmileys(arr) {
+  const collection = [':\\)', ':D', ':~\\)', ':-\\)', ':~D', ':-D', ';\\)', ';D', ';~\\)', ';-\\)', ';~D', ';-D'];
+
+  return arr.filter(
+    el =>
+      el.search(collection[0]) != -1 ||
+      el.search(collection[1]) != -1 ||
+      el.search(collection[2]) != -1 ||
+      el.search(collection[3]) != -1 ||
+      el.search(collection[4]) != -1 ||
+      el.search(collection[5]) != -1 ||
+      el.search(collection[6]) != -1 ||
+      el.search(collection[7]) != -1 ||
+      el.search(collection[8]) != -1 ||
+      el.search(collection[9]) != -1 ||
+      el.search(collection[10]) != -1 ||
+      el.search(collection[11]) != -1
+  ).length;
+}
+
+//!
+function countSmileys(arr) {
+  return arr.filter(x => /^[:;][-~]?[)D]$/.test(x)).length;
+}
+
+console.log(countSmileys([':D', ':~)', ';~D', ':)'])); // 4
