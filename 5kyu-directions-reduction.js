ff50537781c001ec295d88b67a1959788a830c12
ZnoGouDj <znogoud3@gmail.com>
2022-03-03 16:01:33 +0300

    6kyu

--- 5kyu-directions-reduction.js
+++ 5kyu-directions-reduction.js
@@ -1,19 +1,25 @@
 function dirReduc(arr) {
-    let arrCopy = [...arr];
+  let arrCopy = [...arr];
 
-    for (let i = 0; i < arrCopy.length; i++) {
-        if (arrCopy[i] === "NORTH" && arrCopy[i + 1] === "SOUTH" || arrCopy[i] === "SOUTH" && arrCopy[i + 1] === "NORTH") {
-            arrCopy.splice(i, 2);
-            i = -1;
-        } else if (arrCopy[i] === "EAST" && arrCopy[i + 1] === "WEST" || arrCopy[i] === "WEST" && arrCopy[i + 1] === "EAST") {
-            arrCopy.splice(i, 2);
-            i = -1;
-        } 
+  for (let i = 0; i < arrCopy.length; i++) {
+    if (
+      (arrCopy[i] === 'NORTH' && arrCopy[i + 1] === 'SOUTH') ||
+      (arrCopy[i] === 'SOUTH' && arrCopy[i + 1] === 'NORTH')
+    ) {
+      arrCopy.splice(i, 2);
+      i = -1;
+    } else if (
+      (arrCopy[i] === 'EAST' && arrCopy[i + 1] === 'WEST') ||
+      (arrCopy[i] === 'WEST' && arrCopy[i + 1] === 'EAST')
+    ) {
+      arrCopy.splice(i, 2);
+      i = -1;
     }
-    
-    return arrCopy;
+  }
+
+  return arrCopy;
 }
 
-console.log(dirReduc(["NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"])); // ["WEST"]
-console.log(dirReduc(["NORTH", "WEST", "SOUTH", "EAST"])); // ["NORTH", "WEST", "SOUTH", "EAST"]
-console.log(dirReduc(["NORTH", "SOUTH", "EAST", "WEST", "EAST", "WEST"])); // []
+console.log(dirReduc(['NORTH', 'SOUTH', 'SOUTH', 'EAST', 'WEST', 'NORTH', 'WEST'])); // ["WEST"]
+console.log(dirReduc(['NORTH', 'WEST', 'SOUTH', 'EAST'])); // ["NORTH", "WEST", "SOUTH", "EAST"]
+console.log(dirReduc(['NORTH', 'SOUTH', 'EAST', 'WEST', 'EAST', 'WEST'])); // []
---
+++ 6kyu-title-case.js
@@ -0,0 +1,36 @@
+function titleCase(title, minorWords) {
+  if (!minorWords)
+    return title
+      .split(' ')
+      .map(el => capitalizeFirstLetter(el))
+      .join(' ');
+
+  let arr = title.split(' ');
+  let minorArr = minorWords.split(' ');
+  let result = [capitalizeFirstLetter(arr[0])];
+
+  let match;
+
+  for (let i = 1; i < arr.length; i++) {
+    match = false;
+    for (let j = 0; j < minorArr.length; j++) {
+      if (arr[i].toLowerCase() === minorArr[j].toLowerCase()) {
+        match = true;
+        result.push(arr[i].toLowerCase());
+      }
+    }
+    if (!match) {
+      result.push(capitalizeFirstLetter(arr[i]));
+    }
+  }
+
+  return result.join(' ');
+}
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+console.log(titleCase('a clash of KINGS', 'a an the of')); // should return: 'A Clash of Kings'
+console.log(titleCase('THE WIND IN THE WILLOWS', 'The In')); // should return: 'The Wind in the Willows'
+console.log(titleCase('the quick brown fox')); // should return: 'The Quick Brown Fox')
