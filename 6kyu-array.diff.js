7ff20a214260cd96392ed3abdaa1fbdb4094a12a
ZnoGouDj <znogoud3@gmail.com>
2022-03-08 19:10:04 +0100

    6kyu=x=2

---
+++ 6kyu-array.diff.js
@@ -0,0 +1,8 @@
+function arrayDiff(a, b) {
+  return a.filter(x => !b.includes(x));
+}
+
+console.log(arrayDiff([], [4, 5])); // []
+console.log(arrayDiff([3, 4], [3])); // [4]
+console.log(arrayDiff([1, 8, 2], [])); // [1, 8, 2]
+console.log(arrayDiff([1, 2, 3], [1, 2])); // [3]
---
+++ 6kyu-detect-pangram.js
@@ -0,0 +1,56 @@
+function isPangram(string) {
+  let str = string.toUpperCase().split(' ').join('');
+
+  const alphabet = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (str.includes(alphabet[i])) {
+      alphabet.splice(i, 1);
+      i = -1;
+    }
+  }
+
+  return !alphabet.length;
+}
+
+//!
+
+function isPangram(string) {
+  string = string.toLowerCase();
+  return 'abcdefghijklmnopqrstuvwxyz'.split('').every(function (x) {
+    return string.indexOf(x) !== -1;
+  });
+}
+
+console.log(isPangram('The quick brown fox jumps over the lazy dog.')); //true
+console.log(isPangram('This is not a pangram.')); //false
+
+console.assert(isPangram('The quick brown fox jumps over the lazy dog.') === true);
+console.assert(isPangram('This is not a pangram.') === false);
