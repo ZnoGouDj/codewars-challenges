8c92f3d1cc99ba3ed527e48fd3316e2fd0940ff1
ZnoGouDj <znogoud3@gmail.com>
2022-03-16 19:05:37 +0100

    6kyu

---
+++ 6kyu-valid-braces.js
@@ -0,0 +1,35 @@
+function validBraces(braces) {
+  let arr = braces.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '(' && arr[i + 1] === ')') {
+      arr.splice(i, 2);
+      i = -1;
+    } else if (arr[i] === '[' && arr[i + 1] === ']') {
+      arr.splice(i, 2);
+      i = -1;
+    } else if (arr[i] === '{' && arr[i + 1] === '}') {
+      arr.splice(i, 2);
+      i = -1;
+    }
+  }
+
+  return !arr.length;
+}
+
+//!
+function validBraces(braces) {
+  while (braces.indexOf('{}') != -1 || braces.indexOf('()') != -1 || braces.indexOf('[]') != -1) {
+    braces = braces.replace('{}', '').replace('()', '').replace('[]', '');
+  }
+  return braces.length == 0;
+}
+
+console.log(validBraces('()))')); // false
+console.log(validBraces('()')); // true
+console.log(validBraces('[]')); // true
+console.log(validBraces('{}')); // true
+console.log(validBraces('(){}[]')); // true
+console.log(validBraces('([{}])')); // true
+console.log(validBraces('(}')); // false
+console.log(validBraces('[(])')); // false
