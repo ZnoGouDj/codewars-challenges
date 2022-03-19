faef0032f1c3c42c8f1f8399a98417931a7dfb87
ZnoGouDj <znogoud3@gmail.com>
2022-03-19 22:25:06 +0100

    7kyu

---
+++ 7kyu-parts-of-a-list.js
@@ -0,0 +1,28 @@
+function partlist(arr) {
+  let array = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    array.push([arr.slice(0, i + 1).join(' ')]);
+    array[i][1] = arr.slice(i + 1).join(' ');
+  }
+  return array;
+}
+
+//!
+var partlist = a => a.map((v, i) => [a.slice(0, i).join(' '), a.slice(i).join(' ')]).slice(1);
+
+console.log(partlist(['I', 'wish', 'I', "hadn't", 'come'])); /* [
+  ['I', "wish I hadn't come"],
+  ['I wish', "I hadn't come"],
+  ['I wish I', "hadn't come"],
+  ["I wish I hadn't", 'come'],
+]); */
+console.log(partlist(['cdIw', 'tzIy', 'xDu', 'rThG'])); /* [
+  ['cdIw', 'tzIy xDu rThG'],
+  ['cdIw tzIy', 'xDu rThG'],
+  ['cdIw tzIy xDu', 'rThG'],
+]); */
+console.log(partlist(['vJQ', 'anj', 'mQDq', 'sOZ'])); /* [
+  ['vJQ', 'anj mQDq sOZ'],
+  ['vJQ anj', 'mQDq sOZ'],
+  ['vJQ anj mQDq', 'sOZ'],
+]); */
