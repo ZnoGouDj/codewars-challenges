5bf1432163e2ec15d9b713894598c1436f500fcb
ZnoGouDj <znogoud3@gmail.com>
2022-03-21 22:22:16 +0100

    5 kyu

--- 5kyu-find-the-smallest.js
+++ 5kyu-find-the-smallest.js
@@ -1,30 +1,26 @@
 function smallest(n) {
   let arr = String(n).split('');
-  let replacedNumIndex;
-  let replacedPosition = 0;
-  let arrayToReturn;
+  let indexFrom = 0;
+  let indexTo = 0;
+  let minNum = n;
 
-  for (let i = 0; i < 10; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] == i && j !== 0) {
-        replacedNumIndex = j;
-        let array0 = [...arr];
-        array0.splice(j, 1);
-        array0.unshift(arr[j]);
-        let array1 = [...arr];
-        array1.splice(j, 1);
-        array1.splice(1, 0, arr[j]);
-        if (array0 > array1) {
-          replacedPosition = 1;
-          arrayToReturn = array1.join('');
-        } else {
-          arrayToReturn = array0.join('');
-        }
+  for (let j = 0; j < arr.length; j++) {
+    for (let i = 0; i < arr.length; i++) {
+      let copy = [...arr];
+      let number = copy[i];
 
-        return [arrayToReturn, replacedNumIndex, replacedPosition];
+      copy.splice(i, 1);
+      copy.splice(j, 0, number);
+
+      if (+copy.join('') < minNum || (+copy.join('') === minNum && indexFrom > i)) {
+        minNum = +copy.join('');
+        indexFrom = i;
+        indexTo = j;
       }
     }
   }
+
+  return [minNum, indexFrom, indexTo];
 }
 
 console.log(smallest(261235)); // [126235, 2, 0]
