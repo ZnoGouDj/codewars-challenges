c67d1b3f4ce11c5056f6099959343eb59e4e9393
ZnoGouDj <znogoud3@gmail.com>
2022-04-21 14:11:04 +0200

    6kyu

--- 6kyu/6kyu-your-order-please.ts
+++ 6kyu/6kyu-your-order-please.ts
@@ -1,4 +1,4 @@
-function order(words) {
+export function order(words: string): string {
   // if (!words) return '';
   let arr = words.split(' ');
   let result = [];
@@ -28,6 +28,11 @@ function order(words) {
   return result.join(' ');
 }
 
-console.log(order('is2 Thi1s T4est 3a')); // "Thi1s is2 3a T4est"
-console.log(order('4of Fo1r pe6ople g3ood th5e the2')); // "Fo1r the2 g3ood 4of th5e pe6ople"
-console.log(order('')); // ""
+//!
+
+export function order1(words: String): String {
+  return words
+    .split(' ')
+    .sort((a, b) => +a.match(/\d/) - +b.match(/\d/))
+    .join(' ');
+}
