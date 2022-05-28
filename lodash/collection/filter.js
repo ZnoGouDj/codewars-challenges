49bd0a544dbc0ce6278159467692721f55e54462
ZnoGouDj <znogoud3@gmail.com>
2022-05-28 22:44:24 +0200

    filter with switch statement

--- lodash/collection/filter.js
+++ lodash/collection/filter.js
@@ -1,5 +1,67 @@
+// function filter(collection, predicate) {
+//   let result = [];
+
+//   if (typeof predicate === 'function') {
+//     for (let i = 0; i < collection.length; i++) {
+//       if (predicate(collection[i])) {
+//         result.push(collection[i]);
+//       }
+//     }
+//   } else if (typeof predicate === 'object') {
+//     if (Array.isArray(predicate)) {
+//       for (let i = 0; i < collection.length; i++) {
+//         if (collection[i][predicate[0]] === predicate[1]) {
+//           result.push(collection[i]);
+//         }
+//       }
+//     } else {
+//       for (let i = 0; i < collection.length; i++) {
+//         if (JSON.stringify(collection[i]).includes(JSON.stringify(predicate).replace('}', '').replace('{', ''))) {
+//           result.push(collection[i]);
+//         }
+//       }
+//     }
+//   } else {
+//     for (let i = 0; i < collection.length; i++) {
+//       if (collection[i][predicate]) {
+//         result.push(collection[i]);
+//       }
+//     }
+//   }
+
+//   return result;
+// }
+
 function filter(collection, predicate) {
-  return collection;
+  let result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    switch (typeof predicate) {
+      case 'function':
+        if (predicate(collection[i])) {
+          result.push(collection[i]);
+        }
+        break;
+      case 'object':
+        if (Array.isArray(predicate)) {
+          if (collection[i][predicate[0]] === predicate[1]) {
+            result.push(collection[i]);
+          }
+        } else {
+          if (JSON.stringify(collection[i]).includes(JSON.stringify(predicate).slice(1, -1))) {
+            result.push(collection[i]);
+          }
+        }
+        break;
+      default:
+        if (collection[i][predicate]) {
+          result.push(collection[i]);
+        }
+        break;
+    }
+  }
+
+  return result;
 }
 
 // Iterates over elements of collection, returning an array of all elements predicate returns truthy for
