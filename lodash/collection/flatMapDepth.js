3c9be2cf7916b56aff7bb90fd7792d20514afd5a
ZnoGouDj <znogoud3@gmail.com>
2022-05-30 17:00:39 +0200

    flatMapDepth

--- lodash/collection/flatMapDepth.js
+++ lodash/collection/flatMapDepth.js
@@ -1,5 +1,21 @@
 function flatMapDepth(collection, iteratee, depth = 1) {
-  return collection;
+  let result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    result = result.concat(iteratee(collection[i]));
+  }
+
+  if (depth <= 1) return result;
+
+  let res = result.reduce((acc, val) => acc.concat(val), []);
+  depth -= 2;
+
+  while (depth) {
+    res = res.reduce((acc, val) => acc.concat(val), []);
+    depth--;
+  }
+
+  return res;
 }
 
 function duplicate(n) {
@@ -7,3 +23,4 @@ function duplicate(n) {
 }
 
 console.log(flatMapDepth([1, 2], duplicate, 2)); // => [[1, 1], [2, 2]]
+console.log(flatMapDepth([1, 2], duplicate, 1));
