90feef873748de070f9ec47289130e5f91b0d5dd
ZnoGouDj <znogoud3@gmail.com>
2022-06-06 20:45:00 +0200

    sampleSize

---
+++ lodash/collection/sampleSize.js
@@ -0,0 +1,20 @@
+function sampleSize(collection, n = 1) {
+  let result = [];
+  let keys = Object.keys(collection);
+  if (n > keys.length) n = keys.length;
+
+  while (n > 0) {
+    let rnd = keys[Math.floor(Math.random() * keys.length)];
+    if (collection[rnd] !== undefined) {
+      result.push(collection[rnd]);
+      delete collection[rnd];
+      n--;
+    }
+  }
+
+  return result;
+}
+
+console.log(sampleSize([1, 2, 3], 2)); // => [3, 1]
+
+console.log(sampleSize([1, 2, 3], 4)); // => [2, 3, 1]
