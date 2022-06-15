e3378e432e9674ac84376f9925c295259760533b
ZnoGouDj <znogoud3@gmail.com>
2022-06-15 19:36:45 +0200

    7kyu

--- 6kyu/test.js
+++ 6kyu/test.js
@@ -1,20 +1,10 @@
-function countBits(number) {
-  return number
-    .toString(2)
-    .split('')
-    .reduce((acc, val) => +acc + +val, 0);
+function accum(s) {
+  return s.split('').reduce((acc, val, i) => {
+    let repeat = val.toUpperCase() + val.repeat(i).toLowerCase() + '-';
+    return acc + repeat;
+  }, '');
 }
 
-// 1234 is 10011010010 in binary, so => 5
-
-// console.log(countBits(0)); // => 0
-// console.log(countBits(4)); // => 1
-// console.log(countBits(7)); // => 3
-// console.log(countBits(9)); // => 2
-// console.log(countBits(10)); // => 2
-// console.log(countBits(1234)); // => 5
-console.log(countBits(5327018930937637));
-
-// 111111000101110001111100100101 // 30
-// 10010111011001110010100111111000101110001111100100101 // 53
-// 0000000000010010111011001110010100111111000101110001111100100101 // 64
+console.log(accum('abcd')); // -> "A-Bb-Ccc-Dddd"
+console.log(accum('RqaEzty')); // -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+console.log(accum('cwAt')); // -> "C-Ww-Aaa-Tttt"
