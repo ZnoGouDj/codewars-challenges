7e5cbee6478027f0ad2e62cee5950197863d24df
Egor <znogoud3@gmail.com>
2022-09-19 11:38:40 +0200

    6k

---
+++ 6kyu/6kyu-power-bind.ts
@@ -0,0 +1,33 @@
+//? genius
+Function.prototype.bind = function (obj) {
+  for (key in obj) {
+    global[key] = obj[key];
+  }
+  return this;
+};
+
+//!
+
+Function.prototype.bind = function (ctx) {
+  const that = this;
+  return function (...args) {
+    const newContext = this === global ? ctx : this;
+    return that.apply(newContext, args);
+  };
+};
+
+var func = function () {
+  return this.prop;
+};
+var obj1 = {
+    prop: 1,
+  },
+  obj2 = {
+    prop: 2,
+  };
+
+func = func.bind(obj1);
+console.log(func()); // Will produce 1
+
+func = func.bind(obj2);
+console.log(func()); // Will also produce 1 :(
